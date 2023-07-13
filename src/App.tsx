038a0d3e5d45ffce62f47ab5b61fd8b90ebe584f
Sarah Warren <sarah.warren@code.berlin>
2023-07-13 15:19:01 +0200

    add weather request by-city feature

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { getWeather } from './getWeather';
 
-function App() {
+const App = () => {
+  const [city, setCity] = useState('')
+
+  const updateCity = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setCity(event.target.value)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.tsx</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+      <input placeholder='city name' onChange={updateCity} value={city}></input>
+      <button onClick={() => getWeather(city)}>Get weather</button>
+    </>
   );
 }
 
