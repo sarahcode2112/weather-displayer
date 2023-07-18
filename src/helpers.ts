61b3bf7df6b774f59cb87e7bb236431592c6ebba
Sarah Warren <sarah.warren@code.berlin>
2023-07-18 14:39:18 +0200

    extract helper to get hourly forecast by day

--- src/helpers.ts
+++ src/helpers.ts
@@ -1,3 +1,6 @@
 export const formatResponse = (responseDatum: string | string[] | string[][]) => JSON
     .stringify(responseDatum)
     .replace(/['"]+/g, '');
+
+export const getHourlyForecastByDay = (response, day: number) => { return formatResponse(response.days[day].hours.map((hour) => [ hour.datetime, hour.temp ]))
+}
