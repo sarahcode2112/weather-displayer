85f926943c9e60cc6052e10bd6936c335c7d082d
Sarah Warren <sarah.warren@code.berlin>
2023-07-20 14:47:33 +0200

    center all

--- src/App.tsx
+++ src/App.tsx
@@ -16,10 +16,10 @@ const App = () => {
   };
 
   return (
-    <>
+    <div className="App">
       <WeatherRequest updateResponse={updateResponse} />
       <WeatherDisplay response={response} />
-    </>
+    </div>
   );
 };
 
