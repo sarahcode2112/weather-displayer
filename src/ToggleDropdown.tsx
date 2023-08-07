44d810dbc1300a0dd78c9a3632ba9d0420953bba
Sarah Warren <sarah.warren@code.berlin>
2023-08-07 15:44:12 +0200

    slight fix for dropdown format

--- src/ToggleDropdown.tsx
+++ src/ToggleDropdown.tsx
@@ -3,18 +3,16 @@ import { formatSingleDateTime } from './helpers';
 export const ToggleDropdown = ({ todayHourlyForecast }) => {
   return (
     <>
-        <div>
-          {todayHourlyForecast[1] && (
-            <>
-              <div>By the hour:</div>
-              {todayHourlyForecast.map((hour) => (
-                <div key={hour.datetime}>
-                  {formatSingleDateTime(hour.datetime)}: {hour.temp}&deg;
-                </div>
-              ))}
-            </>
-          )}
-        </div>
+        <div>By the hour:</div>
+        {todayHourlyForecast[1] && (
+          <>
+            {todayHourlyForecast.map((hour) => (
+              <div key={hour.datetime}>
+                {formatSingleDateTime(hour.datetime)}: {hour.temp}&deg;
+              </div>
+            ))}
+          </>
+        )}
     </>
   );
 };
