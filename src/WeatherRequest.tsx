4f5a5aec037e7499ce2337d940baea22bf97d4e0
Sarah Warren <sarah.warren@code.berlin>
2023-08-07 16:04:44 +0200

    improve unit switch UX

--- src/WeatherRequest.tsx
+++ src/WeatherRequest.tsx
@@ -23,24 +23,23 @@ export const WeatherRequest = ({
       <div className="header-text">
         Weather <strong>Request</strong>
       </div>
+      <input
+        placeholder="City or town name"
+        onChange={updateCity}
+        value={city}
+      ></input>
+      <label htmlFor="unitGroup">American/Metric</label>
+      <input
+        id="unitGroup"
+        type="checkbox"
+        onChange={updateUnitGroup}
+        value={unitGroup}
+      >
+      </input>
       <div>
-        <input
-          placeholder="City or town name"
-          onChange={updateCity}
-          value={city}
-        ></input>
         <button className="getForecastButton" onClick={() => getWeather(city, unitGroup, updateResponse)}>
           Get forecast
         </button>
-      </div>
-      <div>
-        <label htmlFor="unitGroup">American/Metric</label>
-        <input
-          id="unitGroup"
-          type="checkbox"
-          onChange={updateUnitGroup}
-          value={unitGroup}
-        ></input>
       </div>
     </>
   );
