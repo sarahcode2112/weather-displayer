22df8f82948ae07110c34244cffb8569d2cd7925
Sarah Warren <sarah.warren@code.berlin>
2023-08-09 15:50:27 +0200

    improve spacing, responsiveness of hourly forecast

--- src/components/HourlyForecast/HourlyForecastRows.tsx
+++ src/components/HourlyForecast/HourlyForecastRows.tsx
@@ -1,13 +1,26 @@
 import { formatSingleDateTime } from "../../utils/formatters";
 
-export const HourlyForecastRows = ({ hourlyForecast }) => (
-    <>
-        {hourlyForecast.map((hour) => (
-        <div key={hour.datetime}>
-            <div className="hourlyForecastRow">
-            {formatSingleDateTime(hour.datetime)}: {hour.temp}&deg;
-            </div>
+export const HourlyForecastRows = ({ hourlyForecast }) => {
+    const rowsPerColumn = 12;
+  
+    return (
+      <div className="hourly-forecast-container">
+        <div className="hourly-forecast-columns">
+          <div className="hourly-forecast-column">
+            {hourlyForecast.slice(0, rowsPerColumn).map((hour) => (
+              <div key={hour.datetime} className="hourlyForecastRow">
+                {formatSingleDateTime(hour.datetime)}: {hour.temp}&deg;
+              </div>
+            ))}
+          </div>
+          <div className="hourly-forecast-column">
+            {hourlyForecast.slice(rowsPerColumn).map((hour) => (
+              <div key={hour.datetime} className="hourlyForecastRow">
+                {formatSingleDateTime(hour.datetime)}: {hour.temp}&deg;
+              </div>
+            ))}
+          </div>
         </div>
-        ))}
-    </>
-);
+      </div>
+    );
+  };
