776aaf3dfaecc4bfc60076d68f3cfae767975c02
Sarah Warren <sarah.warren@code.berlin>
2023-08-09 15:17:02 +0200

    refactor css file to use tailwind apply

--- src/components/WeatherRequest.tsx
+++ src/components/WeatherRequest.tsx
@@ -29,13 +29,13 @@ export const WeatherRequest = ({
         value={city}
       ></input>
       <div className="mt-3">
-        <span className="metricLabel mr-2 text-sm font-medium">Metric</span>
+        <span className="metricLabel mr-2">Metric</span>
         <label className="relative inline-flex items-center cursor-pointer"
         >
           <input type="checkbox" value="unitGroup" onChange={updateUnitGroup} className="sr-only peer"/>
           <div className="w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600"></div>
         </label>
-        <span className="metricLabel ml-2 text-sm font-medium">Imperial</span>
+        <span className="metricLabel ml-2">Imperial</span>
       </div>
       <div>
         <button className="getForecastButton" onClick={() => getWeather(city, unitGroup, updateResponse)}>
