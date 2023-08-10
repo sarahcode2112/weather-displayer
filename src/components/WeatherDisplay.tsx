6377b8123e6b7666b1eb05ab069812f8fe0415ea
Sarah Warren <sarah.warren@code.berlin>
2023-08-10 16:03:17 +0200

    refactor WeatherDisplay

--- src/components/WeatherDisplay.tsx
+++ src/components/WeatherDisplay.tsx
@@ -1,7 +1,7 @@
-import { HourlyForecast } from "./HourlyForecast/HourlyForecast"
-import { getHourlyForecastByDay } from "../utils/getHourlyForecastByDay"
-import { formatResponse } from "../utils/formatters"
-import { WeatherResponse } from "../types/types"
+import { HourlyForecast } from "./HourlyForecast/HourlyForecast";
+import { getHourlyForecastByDay } from "../utils/getHourlyForecastByDay";
+import { formatResponse } from "../utils/formatters";
+import type { WeatherResponse } from "../types/types";
 
 export const WeatherDisplay = ({ response }: { response: WeatherResponse }) => { 
   const todayHourlyForecast = getHourlyForecastByDay(response, 0)
@@ -9,18 +9,18 @@ export const WeatherDisplay = ({ response }: { response: WeatherResponse }) => {
   
   return (
     <>
-        <div className="weatherDescription">
-          Location: {formatResponse(response.resolvedAddress)}
-        </div>
-        <div className="weatherDescription">
-          Forecast: {formatResponse(response.description)}
-        </div>
-        <HourlyForecast todayHourlyForecast={todayHourlyForecast} tomorrowHourlyForecast={tomorrowHourlyForecast}></HourlyForecast>
-        <div>
-          {/* Full response: {JSON.stringify(response)}   */}
-          {/* display response data as string */}
-        </div>
+      <div className="weatherDescription">
+        Location: {formatResponse(response.resolvedAddress)}
+      </div>
+      <div className="weatherDescription">
+        Forecast: {formatResponse(response.description)}
+      </div>
+      <HourlyForecast todayHourlyForecast={todayHourlyForecast} tomorrowHourlyForecast={tomorrowHourlyForecast}></HourlyForecast>
+      <div>
+        {/* Full response: {JSON.stringify(response)}   */}
+        {/* display response data as string */}
+      </div>
     </>
-)
+  )
 }
 
