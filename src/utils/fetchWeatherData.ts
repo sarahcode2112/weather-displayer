cafef9cc8b6e9ee669e3480d5424d78e5a7aebb1
Sarah Warren <sarah.warren@code.berlin>
2023-08-11 21:05:39 +0200

    put api key in .env file instead of inline

--- src/utils/fetchWeatherData.ts
+++ src/utils/fetchWeatherData.ts
@@ -1,13 +1,17 @@
+import { type WeatherResponse } from "../types/types";
+
 export const fetchWeatherData = (
   city: string,
   unitGroup: string,
-  updateResponse: (stringifiedRes: any) => void,
+  updateResponse: (response: WeatherResponse) => void,
 ): void => {
+  const apiKey = process.env.REACT_APP_API_KEY;
   fetch(
-    `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city}?unitGroup=${unitGroup}&key=GTK8Z96XLPV5HJ3KGXK5E9JP7&contentType=json`,
+    // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
+    `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city}?unitGroup=${unitGroup}&key=${apiKey}&contentType=json`,
   )
     .then(async (res) => await res.json()) // parse response data into JavaScript object
-    .then((objectRes: object) => {
+    .then((objectRes: WeatherResponse) => {
       updateResponse(objectRes);
     }) // save response data in response object
     .catch((error) => {
