3826ae5b4bc9bb901423ae68cf8d2b6e585f4d96
Sarah Warren <sarah.warren@code.berlin>
2023-08-11 15:56:58 +0200

    partially clean hourly forecast components

--- src/components/HourlyForecast/HourlyForecast.tsx
+++ src/components/HourlyForecast/HourlyForecast.tsx
@@ -1,12 +1,22 @@
-import { HourlyForecastRows } from './HourlyForecastRows';
+import React from "react";
+import { HourlyForecastRows } from "./HourlyForecastRows";
 
-export const HourlyForecast = ({ todayHourlyForecast, tomorrowHourlyForecast }) => (
+export const HourlyForecast: React.FC = ({
+  todayHourlyForecast,
+  tomorrowHourlyForecast,
+}) => (
   <div className="flex flex-wrap sm:flex-no-wrap">
-      <div className="weatherDescription w-full sm:w-1/2 p-4">Today by the hour:
-      {todayHourlyForecast[1] && <HourlyForecastRows hourlyForecast={todayHourlyForecast} />}
-      </div>
-      <div className="weatherDescription w-full sm:w-1/2 p-4">Tomorrow by the hour:
-      {tomorrowHourlyForecast[1] && <HourlyForecastRows hourlyForecast={tomorrowHourlyForecast} />}
-      </div>
+    <div className="weatherDescription w-full sm:w-1/2 p-4">
+      Today by the hour:
+      {todayHourlyForecast[1] && (
+        <HourlyForecastRows hourlyForecast={todayHourlyForecast} />
+      )}
+    </div>
+    <div className="weatherDescription w-full sm:w-1/2 p-4">
+      Tomorrow by the hour:
+      {tomorrowHourlyForecast[1] && (
+        <HourlyForecastRows hourlyForecast={tomorrowHourlyForecast} />
+      )}
+    </div>
   </div>
 );
--- src/components/HourlyForecast/HourlyForecastRows.tsx
+++ src/components/HourlyForecast/HourlyForecastRows.tsx
@@ -1,26 +1,27 @@
+import React from "react";
 import { formatSingleDateTime } from "../../utils/formatters";
 
-export const HourlyForecastRows = ({ hourlyForecast }) => {
-    const rowsPerColumn = 12;
-  
-    return (
-      <div className="hourly-forecast-container">
-        <div className="hourly-forecast-columns">
-          <div className="hourly-forecast-column">
-            {hourlyForecast.slice(0, rowsPerColumn).map((hour) => (
-              <div key={hour.datetime} className="hourlyForecastRow">
-                {formatSingleDateTime(hour.datetime)}: {hour.temp}&deg;
-              </div>
-            ))}
-          </div>
-          <div className="hourly-forecast-column">
-            {hourlyForecast.slice(rowsPerColumn).map((hour) => (
-              <div key={hour.datetime} className="hourlyForecastRow">
-                {formatSingleDateTime(hour.datetime)}: {hour.temp}&deg;
-              </div>
-            ))}
-          </div>
+export const HourlyForecastRows: React.FC = ({ hourlyForecast }) => {
+  const rowsPerColumn = 12;
+
+  return (
+    <div className="hourly-forecast-container">
+      <div className="hourly-forecast-columns">
+        <div className="hourly-forecast-column">
+          {hourlyForecast.slice(0, rowsPerColumn).map((hour) => (
+            <div key={hour.datetime} className="hourlyForecastRow">
+              {formatSingleDateTime(hour.datetime)}: {hour.temp}&deg;
+            </div>
+          ))}
+        </div>
+        <div className="hourly-forecast-column">
+          {hourlyForecast.slice(rowsPerColumn).map((hour) => (
+            <div key={hour.datetime} className="hourlyForecastRow">
+              {formatSingleDateTime(hour.datetime)}: {hour.temp}&deg;
+            </div>
+          ))}
         </div>
       </div>
-    );
-  };
+    </div>
+  );
+};
