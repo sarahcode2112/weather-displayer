3631c2c310cdd9cf1c98fee529fad6a4e77c3ff0
Sarah Warren <sarah.warren@code.berlin>
2023-08-11 14:47:02 +0200

    clean up App.test.tsx partially

--- src/tests/App.test.tsx
+++ src/tests/App.test.tsx
@@ -1,7 +1,6 @@
-import App from '../App';
-import { render, screen, fireEvent, waitFor } from '@testing-library/react';
-import '@testing-library/jest-dom/extend-expect';
-
+import App from "../App";
+import { render, screen, fireEvent, waitFor } from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
 
 test(`renders 'Today by the hour' title`, () => {
   render(<App />);
@@ -18,18 +17,21 @@ test(`renders 'Forecast:' title`, () => {
 test('Renders "Berlin, Deutschland" after typing "Berlin" and clicking "Get forecast" button', async () => {
   render(<App />);
 
-  jest.setTimeout(10000)
+  jest.setTimeout(10000);
 
-  const locationInput = screen.getByPlaceholderText('City or town name');
-  const getForecastButton = screen.getByText('Get forecast');
+  const locationInput = screen.getByPlaceholderText("City or town name");
+  const getForecastButton = screen.getByText("Get forecast");
 
   // User enters "Berlin" in the location search box
-  fireEvent.change(locationInput, { target: { value: 'Berlin' } });
+  fireEvent.change(locationInput, { target: { value: "Berlin" } });
 
   // User clicks the "Get Forecast" button
   fireEvent.click(getForecastButton);
 
   // Assert that the element with the text "Berlin, Deutschland" appears in the document, after waiting for it to appear
-  const forecastElement = await waitFor(() => screen.getByText(/Berlin, Deutschland/), { timeout: 10000});
+  const forecastElement = await waitFor(
+    () => screen.getByText(/Berlin, Deutschland/),
+    { timeout: 10000 },
+  );
   expect(forecastElement).toBeInTheDocument();
 });
