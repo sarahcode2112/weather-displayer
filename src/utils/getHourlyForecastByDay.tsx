f73c95ff471608aefb3d73e30b5e0e827e7a37db
Sarah Warren <sarah.warren@code.berlin>
2023-08-11 16:05:22 +0200

    refactor getHourlyForecastByDay

--- src/utils/getHourlyForecastByDay.tsx
+++ src/utils/getHourlyForecastByDay.tsx
@@ -1,9 +1,8 @@
-import { WeatherResponse } from "../types/types"
+import { type WeatherResponse } from "../types/types";
 
-export const getHourlyForecastByDay = (response: WeatherResponse, day: number): { datetime: string; temp: string; }[] | null => {
-    if (response.days[day] && response.days[day].hours) {
-      return response.days[day]?.hours
-    } else {
-      return null;
-    }
-  };
+export const getHourlyForecastByDay = (
+  response: WeatherResponse,
+  day: number,
+): Array<{ datetime: string; temp: string }> | null => {
+  return response.days[day]?.hours;
+};
