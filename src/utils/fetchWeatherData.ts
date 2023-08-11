63ab5bacb84408702e32c1c6d803f7d1a59b3aa3
Sarah Warren <sarah.warren@code.berlin>
2023-08-11 16:02:02 +0200

    refactor fetchWeatherData

--- src/utils/fetchWeatherData.ts
+++ src/utils/fetchWeatherData.ts
@@ -1,10 +1,19 @@
-export const fetchWeatherData = (city: string, unitGroup: string, updateResponse: (stringifiedRes: any) => void) => {
-    fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city}?unitGroup=${unitGroup}&key=GTK8Z96XLPV5HJ3KGXK5E9JP7&contentType=json`)
-        .then((res) => res.json()) // parse response data into JavaScript object
-        .then((objectRes: object) => updateResponse(objectRes)) // save response data in response object
-        .catch((error) => {
-            alert('The weather request did not work. Did you type a city name (without typos)?');
-            console.log(error)
-        }
-    )
-}
+export const fetchWeatherData = (
+  city: string,
+  unitGroup: string,
+  updateResponse: (stringifiedRes: any) => void,
+): void => {
+  fetch(
+    `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city}?unitGroup=${unitGroup}&key=GTK8Z96XLPV5HJ3KGXK5E9JP7&contentType=json`,
+  )
+    .then(async (res) => await res.json()) // parse response data into JavaScript object
+    .then((objectRes: object) => {
+      updateResponse(objectRes);
+    }) // save response data in response object
+    .catch((error) => {
+      alert(
+        "The weather request did not work. Did you type a city name (without typos)?",
+      );
+      console.log(error);
+    });
+};
